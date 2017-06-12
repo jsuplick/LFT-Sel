b9f6b5da1b261ae99e51193d1898f56c5046f911
Orasi_SA <orasisa@orasi.com>
2017-06-12 10:21:03 -0400

    initial project set up

--- src/main/java/com/orasi/LFT_Sel.java
+++ src/main/java/com/orasi/LFT_Sel.java
@@ -32,7 +32,7 @@ public class LFT_Sel {
     public static WebDriver driver = null;
     public static WebDriverWait waitVar = null;
 
-    public static String baseURL = "http://orasi.com/";
+    public static String baseURL = "http://advantageonlineshopping.com/#/";
 
     public LFT_Sel() {
     //Change this constructor to private if you supply your own public constructor
@@ -49,13 +49,15 @@ public class LFT_Sel {
         Reporter.init (rconfig);
 
         //Get WebDriver going
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jean\\DevTestStuff\\Selenium_Chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Orasi_SA\\Desktop\\WebDriver\\chromedriver.exe");
         driver = new ChromeDriver();
 
     }
 
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
+
+        driver.quit();
     }
 
     @Before
@@ -64,10 +66,50 @@ public class LFT_Sel {
 
     @After
     public void tearDown() throws Exception {
+
     }
 
     @Test
     public void test() throws Exception {
 
+        driver.manage().window().maximize();
+        driver.get(baseURL);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        //Click the Speakers link
+        WebElement element = driver.findElements(new ByEach(
+                org.openqa.selenium.By.cssSelector("div#SpeakersImg > div > span"),
+                org.openqa.selenium.By.xpath("//*[@id='SpeakersImg']/div/span")
+        )).get(0);
+        element.click();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        //Number of items
+        element = driver.findElements(new ByEach(
+                org.openqa.selenium.By.cssSelector("html > body > div:nth-child(8) > section > article > div:nth-child(4) > div > div > div:nth-child(1) > p > a"),
+                org.openqa.selenium.By.xpath("/html/body/div[4]/section/article/div[3]/div/div/div[1]/p/a")
+        )).get(0);
+
+        if (element.getText().contains("Links")) {
+            System.out.println("Speakers not available");
+        } else {
+            System.out.println("Speakers available");
+        }
+
+        //Select the item
+        element = driver.findElements(new ByEach(
+                org.openqa.selenium.By.cssSelector("body > div.uiview.ng-scope > section > article > div.categoryData > a > button"),
+                org.openqa.selenium.By.xpath("/html/body/div[4]/section/article/div[1]/a/button")
+        )).get(0);
+        element.click();
+
+        // back to home
+        element = driver.findElements(new ByEach(
+                org.openqa.selenium.By.xpath("/html/body/header/nav/div/a/span[1]")
+        )).get(0);
+        element.click();
+
+
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 }
